65c076f8ce836277ba35d4acc19b2b73a158a4a1
Wierin <guillaume.hartenstein@gmail.com>
2016-01-05 14:38:05 +0100

    Ajout de la defense

--- src/serveur/element/Monstre.java
+++ src/serveur/element/Monstre.java
@@ -9,24 +9,25 @@ public class Monstre extends Personnage {
 	private static final long serialVersionUID = 1L;
 	
 	/**
-	 * Carcteristiques de base
+	 * Carcteristiques de base du monstre
 	 */
 	private static final HashMap<Caracteristique, Integer> CARACTS;
 	
 	/**
 	 * Initialisation de la HashMap
 	 */
-	static
-	{
+	static {
 		CARACTS = new HashMap<Caracteristique, Integer>();
 		CARACTS.put(Caracteristique.VIE, 10);
 		CARACTS.put(Caracteristique.FORCE, 10);
 		CARACTS.put(Caracteristique.INITIATIVE, Calculs.valeurCaracAleatoire(Caracteristique.INITIATIVE));
-		//TODO CARACTS.put(Caracteristique.DEFENSE, 0);
+		CARACTS.put(Caracteristique.DEFENSE, 0);
 	}
 
+	/**
+	 * Construit un monstre
+	 */
 	public Monstre() {
 		super("Monstre", "Serveur", CARACTS);
 	}
-
 }
