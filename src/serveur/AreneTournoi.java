06422180883fcc92d97ae132ee440d0a1c2702bb
demarage <philippe.asselbergh@gmail.com>
2016-01-05 15:53:36 +0100

    groupeDemarrage: tous les personnages (9 ou 10 par poule) sont placés en cercle

--- src/serveur/AreneTournoi.java
+++ src/serveur/AreneTournoi.java
@@ -3,6 +3,8 @@ package serveur;
 import java.awt.Point;
 import java.rmi.RemoteException;
 import java.rmi.UnmarshalException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import client.controle.IConsole;
@@ -20,7 +22,8 @@ import utilitaires.Constantes;
  *
  */
 public class AreneTournoi extends Arene {
-
+	private static List<Point> lstCoord = new ArrayList<>();;
+	private int numeroPersonnage = 0;
 	private static final long serialVersionUID = 1L;
 	
 	/**
@@ -51,7 +54,7 @@ public class AreneTournoi extends Arene {
 			motDePasse = sc.nextLine();
 			sc.close();
 		}
-		
+		initList(10);
 		partieCommencee = false;
 	}
 	
@@ -78,15 +81,24 @@ public class AreneTournoi extends Arene {
 		
 		int portConsole = port + refRMI;
 		String adr = Constantes.nomRMI(ipConsole, portConsole, "Console" + refRMI);
-		
-		if(partieCommencee) {
+		if(numeroPersonnage < 10){
+			if(partieCommencee) {
+				// refus si la partie a commence
+				res = false;
+				
+				logger.info(Constantes.nomClasse(this), 
+						"Demande de connexion refusee (partie deja commencee) (" + adr + ")");
+			} else {
+				position = lstCoord.get(numeroPersonnage);
+				numeroPersonnage += 1;
+				res = super.connecte(refRMI, ipConsole, personnage, nbTours, position);
+			}
+		}else{
 			// refus si la partie a commence
 			res = false;
 			
 			logger.info(Constantes.nomClasse(this), 
-					"Demande de connexion refusee (partie deja commencee) (" + adr + ")");
-		} else {
-			res = super.connecte(refRMI, ipConsole, personnage, nbTours, position);
+					"Demande de connexion refusee (nombre de jouers = 10) (" + adr + ")");
 		}
 		
 		return res;
@@ -186,5 +198,18 @@ public class AreneTournoi extends Arene {
 		}
 		
 		return msg;
+	}
+	public void initList(int points){
+		int x = 50;
+		int y = 50;
+		int radius = 40;
+		double slice = 2 * Math.PI / points;
+	    for (int i = 0; i < points; i++)
+	    {
+	        double angle = slice * i;
+	        int newX = (int)(x + radius * Math.cos(angle));
+	        int newY = (int)(y + radius * Math.sin(angle));
+	        lstCoord.add(new Point(newX, newY));
+	    }
 	}
 }
